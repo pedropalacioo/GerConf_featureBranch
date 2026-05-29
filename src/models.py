a28a83ed709f397e7165a619d097b81acee62e01
Samuel Silveira <samuelsilveira100@gmail.com>
2026-05-28 23:53:25 -0300

    feat: implementa classe de modelo Usuario com suporte a histórico

--- src/models.py
+++ src/models.py
@@ -42,3 +42,40 @@ class Livro:
             genero=data['genero'],
             disponivel=data.get('disponivel', True)
         )
+
+class Usuario:
+    """Modelo representando um usuário do sistema."""
+    
+    def __init__(self, id, nome, username, senha, historico=None):
+        self.id = id
+        self.nome = nome
+        self.username = username
+        self.senha = senha
+        self.historico = historico if historico is not None else []
+    
+    def __str__(self):
+        return f"[{self.id}] {self.nome} (@{self.username}) | Empréstimos: {len(self.historico)}"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def to_dict(self):
+        """Converte o usuário para dicionário."""
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'username': self.username,
+            'senha': self.senha,
+            'historico': self.historico
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        """Cria um usuário a partir de um dicionário."""
+        return Usuario(
+            id=data['id'],
+            nome=data['nome'],
+            username=data['username'],
+            senha=data['senha'],
+            historico=data.get('historico', [])
+        )
